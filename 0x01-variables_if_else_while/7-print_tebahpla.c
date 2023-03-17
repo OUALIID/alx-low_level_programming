f7911d29cf4e990a83302dfef6c798c658493559
OUALID ELHADIM <96590775+OUALIID@users.noreply.github.com>
2023-03-17 09:35:29 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,22 @@
-i
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}
