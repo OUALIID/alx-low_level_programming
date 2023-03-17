8cbfdcff047e495218111dcf8b9d65283730322f
OUALID ELHADIM <96590775+OUALIID@users.noreply.github.com>
2023-03-17 09:29:53 +0000

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,22 @@
-fjh
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		putchar(number);
+		number++;
+	}
+	putchar('\n');
+	return (0);
+}
