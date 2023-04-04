6bcb3357825a0cf098219e77f57bc5a7fac291ac
OUALID <walidelhadim8@gmail.com>
2023-04-03 17:45:54 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_memset() - fill a specific memory area with a fixed byte
+ *
+ * @s: pointer to the beginning of the memory area to be filled
+ * @b: The value of the byte to be filled in the memory area
+ * @n: The number of bytes to fill in the memory area
+ *
+ * return: pointer to memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		[i] = b;
+	}
+	return (s);
+{
