eb288cd579d718d63575156636fbd9f5bac57fcd
OUALID <walidelhadim8@gmail.com>
2023-04-05 13:06:59 -0700

    1. Why is it so important to dream? Because, in my dreams we are together

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This results in string outpu
+ * entirely in the opposite direction.
+ *@s: for inpu.
+ *
+ *Return: void.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s++);
+		_putchar(*s);
+	}
+}
