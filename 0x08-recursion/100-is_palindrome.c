2640d85095ad20f38f44cf50a7ff053a0368257a
OUALID <walidelhadim8@gmail.com>
2023-04-05 19:10:08 -0700

    7. They say we only use a fraction of our brains

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,3 +15,26 @@ int _strlen(char *s)
 	s++;
 	return (_strlen(s) + 1);
 }
+
+/**
+ * _compare -> Compare two strings to see if they match or not
+ * @s: the input string to compare
+ * @len: the length of the string that should be compared
+ *
+ * Return: Here we depend on the situation
+ */
+
+
+int _compare(char *s, int len)
+{
+	if (len < 1)
+		return (1);
+	if (*s == *(s + len))
+	{
+		s++;
+		len -= 2;
+		return (_compare(s, len));
+	}
+	return (0);
+}
+
