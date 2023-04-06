f31c0cb71e9c625bcb7d7c690f580d1a8722bc06
OUALID <walidelhadim8@gmail.com>
2023-04-05 17:49:06 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,19 +3,19 @@
 /**
  * wildcmp - Compares two strings and returns
  * a value based on whether or not they match
- * @s1: the first input string
- * @s2: the second input string
+ * @s3: the first input string
+ * @s4: the second input string
  *
  * Return: Here we depend on the situation
  */
 
-int wildcmp(char *s1, char *s2)
+int wildcmp(char *s3, char *s4)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s2 + 1, s1)));
+	if (*s4 == '\0')
+		return (*s3 == '\0');
+	if (*s4 == *s3)
+		return (*s3 != '\0' && wildcmp(s3 + 1, s4 + 1));
+	if (*s4 == '*')
+		return (wildcmp(s3, s4 + 1) || (*s3 != '\0' && wildcmp(s3 + 1, s4)));
 	return (0);
 }
