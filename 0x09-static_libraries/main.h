c5fa1764d66af0aaebd1bdfbe49bd372115730d8
OUALID <walidelhadim8@gmail.com>
2023-04-07 09:11:39 -0700

    0. A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,23 @@
+#define MAIN_H
+#endif
+#ifndef MAIN_H
+int _strlen(char *s);
+char *_strncat(char *dest, char *src, int n);
+char *_strcpy(char *dest, char *src);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+int _atoi(char *s);
+int _isdigit(int c);
+int _putchar(char c);
+int _isalpha(int c);
+char *_strchr(char *s, char c);
+void _puts(char *s);
+unsigned int _strspn(char *s, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+int _isupper(int c);
+int _abs(int n);
+char *_strcat(char *dest, char *src);
+int _islower(int c);
