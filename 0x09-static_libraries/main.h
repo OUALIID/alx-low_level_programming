e69b8d81b8c6d35acecf01f47e291cdd249d1b47
OUALID <walidelhadim8@gmail.com>
2023-04-08 16:18:44 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,23 +1,25 @@
-#define MAIN_H
-#endif
 #ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
 int _strlen(char *s);
-char *_strncat(char *dest, char *src, int x);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char n);
+unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _atoi(char *s);
-int _isdigit(int w);
-int _putchar(char w);
-int _isalpha(int w);
-char *_strchr(char *s, char w);
-void _puts(char *s);
-unsigned int _strspn(char *s, char *accept);
-char *_memset(char *s, char a, unsigned int x);
-char *_memcpy(char *dest, char *src, unsigned int x);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int x);
-int _isupper(int w);
-int _abs(int w);
-char *_strcat(char *dest, char *src);
-int _islower(int w);
+
+#endif
