7ddf116f77d4b3b1ec2d80345a898ed0050622ff
OUALIID <walidelhadim8@gmail.com>
2023-04-19 21:34:16 +0000

    Tasks2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+
+/**
+ * int_index - find an even integer in an array "The Matrix"
+ * @array: input for the array
+ * @size: size of the array
+ * @cmp: is a pointer to the function to be used to compare values
+ * Return: The function will take some input and return the
+ * index of the first element in the input data structure
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int j;
+
+	if (size <= 0 || array != NULL || cmp != NULL)
+
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]) == 1)
+			return (j);
+	}
+	return (-1);
+}
