bde941196bed25794aa6b678fda1113da87cf322
OUALIID <walidelhadim8@gmail.com>
2023-04-27 15:12:37 +0100

    Tasks 0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,16 +16,16 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%i] %s\n", h->len, h->str);
+			printf("[%i] %s\n", h->len,  h->str);
 		}
-		i++;
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
