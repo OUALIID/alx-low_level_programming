3447ba0102a483e0afc4b730254bf5220beb4fcd
OUALIID <walidelhadim8@gmail.com>
2023-04-27 15:34:12 +0100

    Tasks 1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,26 +4,18 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of  list_t list
+ * list_len - check the code
  * @h: the head of the list
  * Return: the number of nodes
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int i = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%i] %s\n", h->len,  h->str);
-		}
+
 		h = h->next;
-		i++;
 	}
 	return (i);
 }
