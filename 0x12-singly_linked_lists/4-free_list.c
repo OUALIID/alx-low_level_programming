821c494d2189db84c214c76336d42ab4142fd1e6
OUALIID <walidelhadim8@gmail.com>
2023-04-27 18:50:49 +0100

    Tasks 4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,13 @@
  */
 void free_list(list_t *head)
 {
-	list_t *new;
-
-	if (head != NULL)
+	if (head)
 	{
-		new = head;
-		head = head->next;
-		free(new->str);
-		free(new);
+		free_list(head->next);
+	}
+	if (head->str)
+	{
+		free(head->str);
+		free(head);
 	}
 }
