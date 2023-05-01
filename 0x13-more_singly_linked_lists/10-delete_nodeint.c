7657b3b88f671cb17b784d5e5902955c3563760a
OUALIID <walidelhadim8@gmail.com>
2023-05-02 00:51:55 +0100

    Tasks 10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	for (i = 0; i < index - 1; i++)
 	{
-		if (new_node->next == NULL && new_node == NULL)
+		if (new_node->next == NULL || new_node == NULL)
 			return (-1);
 			new_node = new_node->next;
 	}
