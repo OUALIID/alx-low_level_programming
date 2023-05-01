5ad982ae76b7e2a561665620de7222169453ee3e
OUALIID <walidelhadim8@gmail.com>
2023-05-01 19:33:07 +0100

    Tasks 7. Get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,12 +9,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	if (head == NULL)
-	for (i=0 ; i < index; ++i)
-		{
-			if (head->next != NULL)
-				head = head->next;
-		}
-		return (head);
+		return (NULL);
+	for (i = 0; i < index; ++i)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
 }
