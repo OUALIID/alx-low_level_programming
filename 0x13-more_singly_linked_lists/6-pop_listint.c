d8cbe76784191f246fea9d2991e066d4c22d4462
OUALIID <walidelhadim8@gmail.com>
2023-05-01 19:01:05 +0100

    Tasks 6. Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - A function that deletes
+ * the head node of a listint_t
+ * @head: is the Input list
+ * Return: deleted the Content
+*/
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *free_ls;
+
+	if (*head != NULL)
+	free_ls = *head;
+	*head = (*head)->next;
+	n = free_ls->n;
+	free(free_ls);
+	return (n);
+}
