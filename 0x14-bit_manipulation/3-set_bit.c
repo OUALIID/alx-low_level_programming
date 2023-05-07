b090ca237c4350fc57b07f1a5d22ced936d28b14
OUALIID <walidelhadim8@gmail.com>
2023-05-07 16:58:10 +0100

    Tasks 3. 11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,10 +4,11 @@
  * @n: is to input numbers
  * @index: is to input index
  * Return: 1 if it worked, or -1 if an error occurred
-*/
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int len = 1;
+
 	if (index > 64)
 	return (-1);
 	len <<= index;
