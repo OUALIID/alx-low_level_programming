15e7890618f08b90731a0eff737ccfa8b078487b
OUALIID <walidelhadim8@gmail.com>
2023-05-09 14:45:02 +0100

    Tasks 1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,20 @@ int create_file(const char *filename, char *text_content)
 	int op, wr, i;
 
 	if (filename == NULL)
-		return (-1);
+	return (-1);
 	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (op < 0)
-		return (-1);
+	return (-1);
 	if (text_content == NULL)
 	{
-		close(op);
-		return (1);
+	close(op);
+	return (1);
 	}
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 	wr = write(op, text_content, i);
 	if (wr < 0)
-		return (-1);
-        close(op);
-        return (1);
+	return (-1);
+	close(op);
+	return (1);
 }
