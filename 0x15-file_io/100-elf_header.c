4b0c316cb84d27acbce072f8415ab8f1129bbc7b
OUALIID <walidelhadim8@gmail.com>
2023-05-12 12:20:56 +0100

    does not work

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,28 +1,29 @@
 #include "main.h"
-/**
- * main -  the entry point
- * Return: the return is always 0
- */
+
 int main(void)
 {
-	int fd;
-	ssize_t nletters;
-	char *filename;
-	char *text_content;
+    int fd;
+    ssize_t nletters;
+    char *filename;
+    char *text_content;
 
-	filename = "test_0.txt";
-	text_content = "Holberton School!\n";
-	nletters = 0;
-	printf("-> %s file created\n", filename);
-	fd = create_file(filename, text_content);
-	printf("-> %s file written\n", filename);
-	nletters = read_textfile(filename, 1024);
-	printf("-> %s file read\n", filename);
-	printf("-> %ld bytes read\n", nletters);
-	printf("-> %s file appended\n", filename);
-	nletters = append_text_to_file(filename, "\nI love C!");
-	printf("-> %ld bytes appended\n", nletters);
-	nletters = read_textfile(filename, 1024);
-	printf("-> %ld bytes read\n", nletters);
-	return (0);
+    filename = "test_0.txt";
+    text_content = "Holberton School!\n";
+    nletters = 0;
+    printf("-> %s file created\n", filename);
+    fd = create_file(filename, text_content);
+    printf("-> %s file written\n", filename);
+    nletters = read_textfile(filename, 1024);
+    printf("-> %s file read\n", filename);
+    printf("-> %ld bytes read\n", nletters);
+    printf("-> %s file appended\n", filename);
+    nletters = append_text_to_file(filename, "\nI love C!");
+    printf("-> %ld bytes appended\n", nletters);
+    nletters = read_textfile(filename, 1024);
+    printf("-> %ld bytes read\n", nletters);
+
+    /* add this line to use the variable fd */
+    printf("-> File descriptor: %d\n", fd);
+
+    return (0);
 }
